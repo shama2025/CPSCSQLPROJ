63c989b13ba1dc4c6cfabb575aa222ed3d604de7
Marcus Shaffer <101298098+shama2025@users.noreply.github.com>
2022-12-05 14:55:57 -0500

    Add files via upload

---
+++ SQLFRONTEND/SQLFRONTEND/Form4.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace SQLFRONTEND
+{
+    partial class Form4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(134, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Title ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(134, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Staff ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(92, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(136, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Book Condition";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(102, 153);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(117, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Type of Book";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(134, 192);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 25);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "ISBN";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(71, 232);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(157, 25);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Author First Name";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(73, 266);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(155, 25);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Author Last Name";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(112, 309);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(107, 25);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Publisher ID";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(129, 350);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(90, 25);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Book Title";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(97, 397);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(131, 25);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "Date Published";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(234, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(150, 31);
+            this.textBox1.TabIndex = 10;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(234, 69);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(150, 31);
+            this.textBox4.TabIndex = 13;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(234, 108);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(150, 31);
+            this.textBox5.TabIndex = 14;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(234, 147);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(150, 31);
+            this.textBox6.TabIndex = 15;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(234, 186);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(150, 31);
+            this.textBox7.TabIndex = 16;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(234, 226);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(150, 31);
+            this.textBox8.TabIndex = 17;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(234, 263);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(150, 31);
+            this.textBox9.TabIndex = 18;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(234, 303);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(150, 31);
+            this.textBox10.TabIndex = 19;
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(234, 344);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(150, 31);
+            this.textBox11.TabIndex = 20;
+            // 
+            // textBox12
+            // 
+            this.textBox12.Location = new System.Drawing.Point(234, 394);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.Size = new System.Drawing.Size(150, 31);
+            this.textBox12.TabIndex = 21;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(536, 372);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 34);
+            this.button1.TabIndex = 22;
+            this.button1.Text = "Enter Book";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox12);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form4";
+            this.Text = "Form4";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private TextBox textBox1;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private TextBox textBox7;
+        private TextBox textBox8;
+        private TextBox textBox9;
+        private TextBox textBox10;
+        private TextBox textBox11;
+        private TextBox textBox12;
+        private Button button1;
+    }
+}
---
+++ SQLFRONTEND/SQLFRONTEND/Form4.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+namespace SQLFRONTEND
+{
+    public partial class Form4 : Form
+    {
+        public Form4()
+        {
+            InitializeComponent();
+        }
+
+
+        private void buildAndDisplay(int pTid, int pSid,string pbook_con, string pbook_type, long pIsbn, string pAuthorF, string pAuthorL, int pPubID, string pbookTitle,  DateTime pPubDate)
+        {
+            try
+            {
+                //Step 1: 
+                //   SQL string is not passed as an argument to the constructor of
+                //   the Command object...SP name will be assigned to the 
+                //   Command object's Text property
+                //   Also, the connection is assigned to the "Connection" property
+                //   of the Command object ... not passed as second argument
+                //   in constructor
+                SqlConnection objConnection = new SqlConnection("Data Source=(local);initial catalog=CPSCBOOK; User ID=sa;Password=SQL2022");
+                SqlCommand objCommand = new SqlCommand();
+
+
+                //Step 2:  Assign property values to Coomand object
+                objCommand.Connection = objConnection;
+                objCommand.CommandText = "FormFour";
+                objCommand.CommandType = CommandType.StoredProcedure;
+                //Name of parameter(s) must match name in SP... data types must also be compatible
+                objCommand.Parameters.AddWithValue("@title_id", pTid);
+                objCommand.Parameters.AddWithValue("@staff_id", pSid);
+                objCommand.Parameters.AddWithValue("@book_condition", pbook_con);
+                objCommand.Parameters.AddWithValue("@book_type", pbook_type); 
+                objCommand.Parameters.AddWithValue("@isbn", pIsbn);
+                objCommand.Parameters.AddWithValue("@author_first", pAuthorF);
+                objCommand.Parameters.AddWithValue("@autho_last", pAuthorL);
+                objCommand.Parameters.AddWithValue("@pub_id", pPubID);
+                objCommand.Parameters.AddWithValue("@book_title", pbookTitle);
+                objCommand.Parameters.AddWithValue("@publish_date", pPubDate);
+                //Step 3: Make DB connection
+                objConnection.Open();
+                //int pTid, int pSid,string pbook_con, string pbook_type, long pIsbn, string pAuthorF, string pAuthorL, int pPubID, string pbookTitle,  DateTime pPubDate
+                //Step 4. Tell DBMS to execute SP .. the command objects "ExecuteNonQuery" method is
+                //used when executing action queries
+                objCommand.ExecuteNonQuery();
+            }
+
+            catch (Exception e)
+            {
+
+                MessageBox.Show(e.ToString());
+            }
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textBox1.Text.Length > 0)
+                {
+                    int Tid = Convert.ToInt32(textBox1.Text);
+                    int Sid = Convert.ToInt32(textBox4.Text);
+                    string book_condition = textBox5.Text;
+                    string book_type = textBox6.Text;
+                    long isbn = Convert.ToInt64(textBox7.Text);
+                    string authorFirst = textBox8.Text;
+                    string authorLast = textBox9.Text;
+                    int publish_Id = Convert.ToInt32(textBox10.Text);
+                    string book_title = textBox11.Text;
+                    DateTime publish_date = Convert.ToDateTime(textBox12.Text);
+                    buildAndDisplay(Tid,Sid,book_condition,book_type,isbn,authorFirst,authorLast,publish_Id,book_title,publish_date);
+                    button1.Enabled = false;
+                    MessageBox.Show("Record Inserted");
+                }
+                else
+                {
+                    MessageBox.Show("ERR in Input");
+                    textBox1.Focus();
+                }
+
+
+            }
+            catch
+            {
+                MessageBox.Show("Enter valid Title_ID");
+                textBox1.Text = "";
+                textBox1.Focus();
+            }
+        }
+    }
+}
